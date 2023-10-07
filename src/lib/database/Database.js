ffd5b72794de6a6ca075d4a9bed42a97f1dca350
Roberto286 <roberto.saliola96@gmail.com>
2023-10-07 11:28:01 +0200

    fix: database initialization

--- src/lib/database/Database.js
+++ src/lib/database/Database.js
@@ -1,8 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
+const sqlScriptPath = './tables_schema.sql'; //TODO -> make it configurable
 
 class Database {
   constructor(dbPath) {
-    this.db = new sqlite3.Database(dbPath, (err) => {
+    this.db = new sqlite3.Database(dbPath, err => {
       if (err) {
         console.error('Error while opening database:', err.message);
       } else {
@@ -14,12 +16,31 @@ class Database {
 
   initializeDatabase() {
     this.db.serialize(() => {
-      this.db.run(/*query di creazione delle tabelle*/);
+      this.db.get('SELECT COUNT(*) AS count FROM champions', (err, row) => {
+        if (err) {
+          console.error(err.message);
+        } else {
+          const { count: rowCount } = row;
+          if (rowCount === 0) {
+            const script = fs.readFileSync(sqlScriptPath, 'utf-8');
+
+            this.db.exec(script, err => {
+              if (err) {
+                console.error(err.message);
+              } else {
+                console.log('Database created');
+              }
+            });
+          } else {
+            console.log('Skipping database creation');
+          }
+        }
+      });
     });
   }
 
   close() {
-    this.db.close((err) => {
+    this.db.close(err => {
       if (err) {
         console.error('Error while closing db connection:', err.message);
       } else {
