c9d555bd3d05ede27474b435f2404d4cc0111235
Roberto286 <roberto.saliola96@gmail.com>
2023-10-07 21:24:12 +0200

    feat: Refactor database constructor and add new methods for retrieving champion data

--- src/lib/database/Database.js
+++ src/lib/database/Database.js
@@ -2,15 +2,22 @@ const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs');
 const path = require('path');
 
-const schemaCreationsScriptPath = path.join(__dirname, './tables_schema.sql'); // TODO: Make it configurable
-
 class Database {
-  constructor(dbPath) {
+  constructor(dbFilePath) {
     if (Database.instance) {
       return Database.instance;
     }
 
-    this.db = new sqlite3.Database(dbPath, err => {
+    this.dbFilePath = dbFilePath;
+    this.db = null;
+
+    this.connect();
+
+    Database.instance = this;
+  }
+
+  connect() {
+    this.db = new sqlite3.Database(this.dbFilePath, err => {
       if (err) {
         console.error('Error while opening database:', err.message);
       } else {
@@ -18,24 +25,21 @@ class Database {
         this.initializeDatabase();
       }
     });
-
-    Database.instance = this;
   }
 
   initializeDatabase() {
-    this.db.get('SELECT COUNT(*) AS count FROM champions', (err, row) => {
-      if (err) {
-        console.error(err.message);
-        this.executeScript(schemaCreationsScriptPath);
-      } else {
-        const { count: rowCount } = row;
+    this.getChampionsCount()
+      .then(rowCount => {
         if (rowCount === 0) {
-          this.executeScript(schemaCreationsScriptPath);
+          this.executeScript(path.join(__dirname, './tables_schema.sql'));
         } else {
           console.log('Skipping database creation');
         }
-      }
-    });
+      })
+      .catch(err => {
+        console.error(err.message);
+        this.executeScript(path.join(__dirname, './tables_schema.sql'));
+      });
   }
 
   executeScript(sqlScriptPath) {
@@ -51,6 +55,58 @@ class Database {
     } catch (error) {
       console.error('Error while reading the script file:', error.message);
     }
+  }
+
+  async getChampionsCount() {
+    return new Promise((resolve, reject) => {
+      this.db.get('SELECT COUNT(*) AS count FROM champions', (err, row) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(row.count);
+        }
+      });
+    });
+  }
+
+  async getChampionBy(fields, values) {
+    return new Promise((resolve, reject) => {
+      if (!Array.isArray(fields) || !Array.isArray(values) || fields.length !== values.length) {
+        reject(new Error('Fields and values must be arrays of the same length.'));
+        return;
+      }
+
+      const whereConditions = fields.map(field => `${field} = ?`).join(' AND ');
+      const query = `SELECT * FROM champions WHERE ${whereConditions}`;
+
+      this.db.get(query, values, (err, row) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(row);
+        }
+      });
+    });
+  }
+
+  async getRandomChampion(role) {
+    let whereClause = '';
+
+    if (role) {
+      whereClause = 'WHERE role = ?';
+    }
+
+    const query = `SELECT * FROM table ${whereClause} ORDER BY RANDOM() LIMIT 1`;
+
+    return new Promise((resolve, reject) => {
+      this.db.get(query, role, (err, row) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(row);
+        }
+      });
+    });
   }
 
   close() {
