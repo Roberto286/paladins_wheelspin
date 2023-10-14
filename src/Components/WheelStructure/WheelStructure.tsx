e2fef15904159046ab05b54c49c1c14fd3f0067b
Roberto286 <roberto.saliola96@gmail.com>
2023-10-14 12:40:57 +0200

    feat: refactor wheelStructure component

--- src/Components/WheelStructure/WheelStructure.tsx
+++ src/Components/WheelStructure/WheelStructure.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import WheelButton from '../WheelButton/WheelButton';
 import WheelSlice from '../WheelSlice/WheelSlice';
-import './WheeleStructure.scss';
-import { wheelRadius, getRandomAngle, sliceCalc } from '../../Utils/Utils';
+import './WheelStructure.scss';
+import { getRandomAngle, sliceCalc, wheelRadius } from '../../Utils/Utils';
 import { IChampion } from '../../Utils/Interfaces';
 import { getAllChampions, getRandomChamp } from '../../Utils/endpointCalls';
 
@@ -10,17 +10,26 @@ function WheelStructure() {
   const [clicked, setClicked] = useState(false);
   const [rotationAngle, setRotationAngle] = useState(0);
   const [displayedValue, setDisplayedValue] = useState<IChampion>();
-  const [champsArray, setChampsArray] = useState<IChampion[]>([]);
+  const [champions, setChampions] = useState<IChampion[]>([]);
   const [slice, setSlice] = useState(0);
+
   const wheelStyle = { transform: `rotate(${rotationAngle}deg)` };
+
+  const fetchData = async () => {
+    const allChampions = await getAllChampions();
+    setChampions(allChampions);
+    setSlice(sliceCalc(allChampions.length));
+  };
 
   const startRotation = async () => {
     const randomChamp = await getRandomChamp();
-    const randomAngle = getRandomAngle(randomChamp.id);
-    const getAngleWithin360 = async () => (await randomAngle) % wheelRadius;
-    setRotationAngle(await getAngleWithin360());
-    document.documentElement.style.setProperty('--stopAngle', `${await randomAngle}deg`);
+    const randomAngle = getRandomAngle(randomChamp.id, champions.length);
+    const angleWithin360 = randomAngle % wheelRadius;
+
+    document.documentElement.style.setProperty('--stopAngle', `${randomAngle}deg`);
     document.documentElement.style.setProperty('--restartAngle', `${rotationAngle}deg`);
+
+    setRotationAngle(angleWithin360);
     setClicked(true);
 
     setTimeout(() => {
@@ -30,52 +39,47 @@ function WheelStructure() {
   };
 
   useEffect(() => {
-    (async () => {
-      const allChampions = await getAllChampions();
-      setChampsArray(allChampions);
-      setSlice(await sliceCalc());
-    })();
+    fetchData();
   }, []);
 
+  const slicesArray = champions.map((champion, index) => (
+    <WheelSlice
+      key={champion.id}
+      championName={champion.name}
+      sliceHeight={slice * index}
+    />
+  ));
+
   return (
-    <>
-      <div className="container">
-        <h1>Paladins wheel of fortune</h1>
-        <div className="board">
-          <div className="spinner-table">
-            <div
-              className={`dial ${clicked ? 'spinning' : ''}`}
-              style={wheelStyle}
-            >
-              {champsArray.map((e, i) => (
-                <WheelSlice
-                  key={e.id}
-                  championName={e.name}
-                  sliceHeight={slice * i}
-                />
-              ))}
-            </div>
-          </div>
-          <div className="arrow">
-            <span className="pointer" />
+    <div className="container">
+      <h1>Paladins wheel of fortune</h1>
+      <div className="board">
+        <div className="spinner-table">
+          <div
+            className={`dial ${clicked ? 'spinning' : ''}`}
+            style={wheelStyle}
+          >
+            {slicesArray}
           </div>
         </div>
-        <WheelButton
-          event={startRotation}
-          isDisabled={clicked}
-        />
+        <div className="arrow">
+          <span className="pointer" />
+        </div>
       </div>
+      <WheelButton
+        event={startRotation}
+        isDisabled={clicked}
+      />
       <div className="display-container">
         {displayedValue ? (
           <>
-            <span className="display-value"> {`${displayedValue.name} won!`}</span>
-            <span className="display-value" /> {/* da aggiungere l'immagine */}
+            <span className="display-value">{`${displayedValue.name} won!`}</span>
+            <span className="display-value" /> {/* Add the image here */}
           </>
-        ) : (
-          ''
-        )}
+        ) : null}
       </div>
-    </>
+    </div>
   );
 }
+
 export default WheelStructure;
