88c8515a199efdf1cfc8c397019f06ee16ead4ed
Roberto286 <roberto.saliola96@gmail.com>
2023-10-14 20:43:39 +0200

    feat: add champion dominant_color to wheel slice + spin button at wheel center

--- src/Components/WheelSlice/WheelSlice.tsx
+++ src/Components/WheelSlice/WheelSlice.tsx
@@ -3,14 +3,23 @@ import './WheelSlice.scss';
 type WheelSliceProps = {
   championName: string;
   sliceHeight: number;
+  color: string;
 };
-function WheelSlice({ championName, sliceHeight }: WheelSliceProps) {
+function WheelSlice({ championName, sliceHeight, color }: WheelSliceProps) {
   return (
     <div
       className="slice"
       style={{ transform: `rotate(${sliceHeight}deg)` }}
     >
+      <span
+        className="before"
+        style={{ borderColor: `transparent transparent ${color} transparent` }}
+      />
       <div className="label">{championName}</div>
+      <span
+        className="after"
+        style={{ borderColor: `transparent ${color} transparent transparent` }}
+      />
     </div>
   );
 }
