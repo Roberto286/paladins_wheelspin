d54f2b3c5c5e0793093b3392ebb17cab4e1db51a
Roberto286 <roberto.saliola96@gmail.com>
2023-10-21 16:28:15 +0200

    feat: mock enabled

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { viteMockServe } from 'vite-plugin-mock';
 
 // https://vitejs.dev/config/
-export default defineConfig(config => {
-  return {
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const isMockEnabled = process.env?.VITE_USE_MOCK === 'true';
+
+  return defineConfig({
     envDir: 'env',
     plugins: [
       react(),
       viteMockServe({
         mockPath: 'mock',
-        enable: true,
+        localEnabled: isMockEnabled,
       }),
     ],
     server: {
@@ -21,5 +24,5 @@ export default defineConfig(config => {
         },
       },
     },
-  };
-});
+  });
+};
