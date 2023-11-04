680300d9ebc80ed66bde3c0f4d752ede948813f2
Roberto286 <roberto.saliola96@gmail.com>
2023-11-04 23:19:58 +0100

    Feat: axios basicAuth + some refactor

--- src/network/endpointCalls.ts
+++ src/network/endpointCalls.ts
@@ -1,28 +1,38 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 import http from 'http';
 import { Champion } from '../interfaces/Champion';
 import urls from './championsUrls';
 
-const axiosConfig = {
-  withCredentials: false, // TODO -> Backend will soon have authentication
+const backendAuthCredential: string = import.meta.env?.VITE_BACKEND_AUTH || '';
+const axiosConfig: AxiosRequestConfig = {
+  withCredentials: true,
   httpAgent: new http.Agent({ keepAlive: true }),
+  headers: {
+    Authorization: `Basic ${backendAuthCredential}`,
+  },
 };
 
 const AXIOS: AxiosInstance = axios.create(axiosConfig);
 
-const fetchAndHandleErrors = async <T>(url: string, errorMessage: string): Promise<T> => {
-  try {
-    const response = await AXIOS.get<T>(url);
-    return response.data;
-  } catch (error) {
-    throw new Error(errorMessage); // TODO -> SHOW AN ALERT WITH ERROR MESSAGE INSTEAD OF THROWING AN ERROR
+AXIOS.interceptors.response.use(
+  response => response.data,
+  error => {
+    // TODO: Show an alert with the error message instead of throwing an error
+    throw new Error(error);
   }
+);
+
+const makeRequest = async <T>(url: string): Promise<T> => {
+  const response: T = await AXIOS.get(url);
+  return response;
 };
 
-export const getAllChampions = async (): Promise<Champion[]> => {
-  return fetchAndHandleErrors<Champion[]>(urls.getAllChampionsURLReversed, 'Error fetching champion data');
+export const getAllChampions = (): Promise<Champion[]> => {
+  const { getAllChampionsURLReversed: url } = urls;
+  return makeRequest<Champion[]>(String(url));
 };
 
-export const getRandomChamp = async (): Promise<Champion> => {
-  return fetchAndHandleErrors<Champion>(urls.getRandomChampionURL, 'Champion data not found in the response');
+export const getRandomChamp = (): Promise<Champion> => {
+  const { getRandomChampionURL: url } = urls;
+  return makeRequest<Champion>(String(url));
 };
