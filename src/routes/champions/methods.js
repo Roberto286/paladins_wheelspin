701455543f77a19207b0cab5fbe409cbe1d4fa5e
Roberto286 <roberto.saliola96@gmail.com>
2023-11-04 23:48:56 +0100

    hotfix: roles query when roles is undefined

--- src/routes/champions/methods.js
+++ src/routes/champions/methods.js
@@ -8,7 +8,7 @@ const database = new Database(databaseFilePath);
 
 async function getRandomChampion(req, res) {
   try {
-    const roles = Array.isArray(req.query.roles) ? req.query.roles : [req.query.roles];
+    const roles = Array.isArray(req.query.roles) && req.query.roles.length ? req.query.roles : [];
     const randomChampion = await database.getRandomChampion(roles);
     const champServerImgPath = path.join(staticFolder, randomChampion.img_path);
     const dominantColor = await getColor(champServerImgPath);
@@ -22,11 +22,11 @@ async function getRandomChampion(req, res) {
 
 async function getAllChampions(req, res) {
   try {
-    const reversed = req.query.reversed;
-    const roles = Array.isArray(req.query.roles) ? req.query.roles : [req.query.roles];
+    const reversed = req.query.reversed === 'true';
+    const roles = Array.isArray(req.query.roles) && req.query.roles.length ? req.query.roles : [];
     const champions = roles.length
-      ? await database.getChampionsByRoles(roles, !!reversed)
-      : await database.getAllChampions(!!reversed);
+      ? await database.getChampionsByRoles(roles, reversed)
+      : await database.getAllChampions(reversed);
 
     await Promise.all(
       champions.map(async champ => {
